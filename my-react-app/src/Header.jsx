e0d3ae97ab8ecfa1d26dad103bf56a400fdcd8bc
CeejayStokes <ceejay.stokes09@gmail.com>
2026-09-21 10:37:44 +0000

    added header

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,7 +1,20 @@
 import './index.css'
 
 function Header() {
-  
+  return (
+    <>
+        <div className="top-nav centerX space-between">
+            <div>
+                <img src="/logo.svg" style={{border:"none", width:"80px", height:"80px",}} />
+            </div>
+            <div>
+                <i className="fa-solid fa-bell cut"  ></i>
+                <i><span className="fa-solid fa-circle-user"></span>Sign in</i>
+            </div>
+
+        </div>
+    </>
+  )
 }
 
 export default Header
